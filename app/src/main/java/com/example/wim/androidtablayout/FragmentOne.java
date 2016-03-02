bfd79438dc7d28b29ab35295364c1117ff6a9b2e
Wim Sonevel <wim.sonevel@yahoo.co.id>
2016-03-02 07:41:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wim/androidtablayout/FragmentOne.java
@@ -0,0 +1,26 @@
+package com.example.wim.androidtablayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by docotel on 3/2/16.
+ */
+public class FragmentOne extends Fragment {
+
+    public static FragmentOne newInstance(){
+        return new FragmentOne();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_one, container, false);
+
+        return rootView;
+    }
+}
